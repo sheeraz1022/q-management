542bc82b63c7b864546116b84fea2bc9bccbc7f7
Sheeraz Shaikh <sheeraz.petarian@gmail.com>
2020-04-11 23:40:34 +0800

    Temp code

---
+++ src/indexOLD.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase/';
+import database from './utils/FirebaseDB.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+import InviteForm from './components/InviteForm';
+import Dashboard from './components/Dashboard';
+import Registration from './components/Registration';
+import UserStatus from './components/UserStatus';
+import NoProcessingCard from './components/NoProcessingCard';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <div>
+        <Route path="/dashboard/:id" component={Dashboard} />
+        <Route path="/registration/:id" component={Registration} />
+        <Route path="/form/:id/:name" component={InviteForm} />
+        <Route path="/status/:id/:userkey" component={UserStatus} />
+        <Route path="/" component={NoProcessingCard} />
+
+      </div>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
