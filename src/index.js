cd7dc42c128d526b7b6ec98eede22ee9af32333d
Sheeraz Shaikh <sheeraz.petarian@gmail.com>
2020-04-11 23:53:23 +0800

    Temp code

--- src/index.js
+++ src/index.js
@@ -1,33 +1,42 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase/';
+import database from './utils/FirebaseDB.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+import InviteForm from './components/InviteForm';
+import Dashboard from './components/Dashboard';
+import Registration from './components/Registration';
+import UserStatus from './components/UserStatus';
+import NoProcessingCard from './components/NoProcessingCard';
 
-const Main = () => <h1>Main!</h1>;
-const Elsewhere = () => <h1>Elsewhere!</h1>;
 
-function App() {
-    return (
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Main</Link>
-                        </li>
-                        <li>
-                            <Link to="/else">Else</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Switch>
-                    <Route path="/" exact component={Main} />
-                    <Route path="/else/" component={Elsewhere} />
-                </Switch>
-            </div>
-        </Router>
-    );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <div>
+        <Route path="/dashboard/:id" component={Dashboard} />
+        <Route path="/registration/:id" component={Registration} />
+        <Route path="/form/:id/:name" component={InviteForm} />
+        <Route path="/status/:id/:userkey" component={UserStatus} />
+        <Route path="/" component={NoProcessingCard} />
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+      </div>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
